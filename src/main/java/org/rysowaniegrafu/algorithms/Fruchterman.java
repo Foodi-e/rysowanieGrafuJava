9202f211be463929792688d75c08ba57006f6910
Me54nger <grzadka.artur@gmail.com>
2026-06-08 21:31:47 +0200

    Update Fruchterman.java

--- src/main/java/org/rysowaniegrafu/algorithms/Fruchterman.java
+++ src/main/java/org/rysowaniegrafu/algorithms/Fruchterman.java
@@ -1,4 +1,14 @@
 package org.rysowaniegrafu.algorithms;
 
+import org.rysowaniegrafu.model.Config;
+import org.rysowaniegrafu.model.Edge;
+import org.rysowaniegrafu.model.Graph;
+import org.rysowaniegrafu.model.Node;
+import org.rysowaniegrafu.model.SelectionMode;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Fruchterman {
 }
