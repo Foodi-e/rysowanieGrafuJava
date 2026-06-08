1ca0d3e10e3419fbecb6fcea5678047c11d1038c
Me54nger <grzadka.artur@gmail.com>
2026-06-08 21:35:38 +0200

    Update Fruchterman.java

--- src/main/java/org/rysowaniegrafu/algorithms/Fruchterman.java
+++ src/main/java/org/rysowaniegrafu/algorithms/Fruchterman.java
@@ -11,4 +11,18 @@ import java.util.List;
 import java.util.Map;
 
 public class Fruchterman {
+    public static void runFruchterman(Graph g){
+        for (Node node : g.nodes) {
+            node.setX(rand.nextDouble() * c.getWidth());
+            node.setY(rand.nextDouble() * c.getHeight());
+        }
+        double k = Math.sqrt((c.getWidth() * c.getHeight()) / n);
+      
+        for (int i = 0; i < c.getIterations() && temp > 0.00001; i++) {
+            temp = loop(nodes, edges, c, k, dx, dy, temp);
+        }
+    }
+    private static void loop(double t, Graph g){
+        
+    }
 }
